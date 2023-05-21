694a4f73f5458b58051feda8b848dff54b950198
ebarroso2214 <ebarroso2214@gmail.com>
2023-05-21 18:18:31 -0400

    added controllers, added models for stage, settime, stageevents, and meetgreet

--- models/stage.js
+++ models/stage.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 const {
   Model
 } = require('sequelize');
@@ -9,18 +9,35 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({Event, StageEvent, SetTime}) {
       // define association here
+      Stage.belongsToMany (Event, {
+        foreignKey: "stage_id",
+        as: "events",
+        through: StageEvent,
+      })
+
+      Stage.hasMany (SetTime, {
+        foreignKey: "stage_id",
+        as: "set_times",
+      })
     }
   }
   Stage.init({
-    stage_id: DataTypes.INTEGER,
-    stage_name: DataTypes.STRING
+    stage_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    stage_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   }, {
     sequelize,
     modelName: 'Stage',
     tableName: 'stages',
     timestamps: false
-  });
-  return Stage;
-};
+  })
+  return Stage
+}
